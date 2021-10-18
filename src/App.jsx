1525e639de8cfe775df4f0a3f820fc4900c568bf
nehhav <nehhav@hotmail.com>
2021-10-18 21:10:20 +0530

    HOC & Layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { Route } from "react-router-dom";
+
+//HOC
+import DefaultHOC from "./HOC/Default.HOC";
+
+//Components
+import Temp from "./components/temp";
+
 function App() {
   return (
-    <div className="App" >
-      <h1>
-
-      </h1>
-
-    </div >
+    <>
+      < DefaultHOC path="/" exact component={Temp} />
+    </>
   );
 }
-
 export default App;
---
+++ src/planning.js
@@ -0,0 +1,21 @@
+// Book my show planning
+
+// We need a reusable carousal
+// Hero Carousel
+//Common carousel
+//cast and crew small carousel
+
+
+//Pages
+//Home page
+//Movie Page
+//Categories Page
+// Auth Page
+//Razorpay payment gateway
+
+//Plan
+//Layouts
+//Routes
+//Pages
+//Components
+
