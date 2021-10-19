fe850ad5ece711ebf07c8a9c893bb31edc5a1fcd
nehhav <nehhav@hotmail.com>
2021-10-19 20:42:48 +0530

    add Poster component

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -2,13 +2,20 @@ import React from "react";
 
 //Components
 import EntertainmentCardSlider from "../components/Entertainment/Entertainmentcard.component";
+import Premier from "../components/Premier/premier.component";
+
+
 
 const HomePage = () => {
     return (
         <>
             <div className="container mx-auto px-4">
-                <h1 className="text-2xl font-bold text-gray-800 my-3">The best of Entertainment</h1>
+                <h1 className="text-2xl font-bold text-gray-800 my-3">
+                    The best of Entertainment
+                </h1>
                 <EntertainmentCardSlider />
+                <Premier />
+
             </div>
         </>
 
